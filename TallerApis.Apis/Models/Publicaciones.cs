2a90baf70131393a96f1631defabf85afae634fa
nach527 <jjaramilloperalta@gmail.com>
2018-10-10 20:55:49 -0500

    creando api

---
+++ TallerApis.Apis/Models/Publicaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    public class Publicaciones: DbContext
+    {
+        public Publicaciones():base("PublicacionesConnection")
+        {
+
+        }
+        public int MyProperty { get; set; }
+    }
+}
