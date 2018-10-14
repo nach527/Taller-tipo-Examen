eec5b54efa1df64685adef774a512122f18b2f9d
nach527 <jjaramilloperalta@gmail.com>
2018-10-14 13:22:12 -0500

    terminando api

---
+++ TallerApis.Apis/Models/PublicacionesContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    public class PublicacionesContext: DbContext
+    {
+
+    }
+}
